4694ca7abc12d51ce2c21a302604149dc79798be
Sam Johnson <idusortus@gmail.com>
2024-01-30 20:27:17 -0600

    resolved previous error by using .HasConversion as described @ https://learn.microsoft.com/en-us/ef/core/modeling/value-conversions?tabs=data-annotations#built-in-converters

--- Database/ApplicationDbContext.cs
+++ Database/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
+using System.Text.Json;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Vertical.API.Entities;
 
 namespace Vertical.API.Database;
@@ -10,7 +12,25 @@ public class ApplicationDbContext : DbContext
     }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<Article>(builder => builder.OwnsOne(a => a.Tags, tagsBuilder => tagsBuilder.ToJson())); // Not sure what Tagsbuilder is
+        // This looks more like JSON but it is rather hard to read
+        modelBuilder.Entity<Article>()
+            .Property(article=>article.Tags)
+            .HasConversion( 
+                v => JsonSerializer.Serialize(v, (JsonSerializerOptions)null),
+                v => JsonSerializer.Deserialize<List<string>>(v, (JsonSerializerOptions)null),
+                new ValueComparer<ICollection<string>>(
+                    (c1, c2) => c1.SequenceEqual(c2),
+                    c => c.Aggregate(0, (a, v) => HashCode.Combine(a, v.GetHashCode())),
+                    c => (ICollection<string>)c.ToList()));                    
+
+        // this works but it adds it as CSV string
+        // modelBuilder.Entity<Article>()
+        //     .Property(article => article.Tags)
+        //     .HasConversion(
+        //         tags => string.Join(',', tags),
+        //         tags => tags.Split(',', StringSplitOptions.RemoveEmptyEntries).ToList()
+        //     );
+        // modelBuilder.Entity<Article>(builder => builder.OwnsOne(a => a.Tags, b => b.ToJson())); // Throws an error: Invalid token type: 'StartObject'.
 
         // // Complex objects as JSON
         // // But what if you want to store the author as JSON? This is also possible with Entity Framework. And not only since version 8,
